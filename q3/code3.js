00d9a23c2408e31b5864671a294e799ce10d9239
IPPain01 <paimigor01@gmail.com>
2026-08-24 08:58:49 -0300

    Include date and time in task list
    Added timestamp to task items when created.

--- q3/code3.js
+++ q3/code3.js
@@ -5,12 +5,23 @@ function addList() {
         return;
     }
 
+    const agora = new Date();
+
+    const dia = String(agora.getDate()).padStart(2, "0");
+    const mes = String(agora.getMonth()+1).padStart(2, "0");
+
+    const hora = String(agora.getHours()).padStart(2, "0");
+    const minutos = String(agora.getMinutes()).padStart(2, "0");
+
+    const horario = `${hora}:${minutos} - ${dia}/${mes}`;
 
     $(".list").append(`<div class="item">
                 <input type="checkbox">
                 <h3> ${title} </h3>
+                <p class="date">${horario}</p>
                 <div class="del">X</div>
             </div>`)
+
     localStorage.setItem("lista", $(".list").html());
     $(".taskName").val("");
 }
@@ -18,16 +29,16 @@ function addList() {
 function delList() {
     $(this).parent(".item").remove();
     localStorage.setItem("lista", $(".list").html());
-
-    $(".list").on("change", "input[type='checkbox']", function() {
-        $(this).attr("checked", this.checked);
-        localStorage.setItem("lista", $(".list").html());
-    });
 }
 
 
 $(".create").on("click", addList);
 $(".list").on("click", ".del", delList);
+
+$(".list").on("change", "input[type='checkbox']", function() {
+        $(this).attr("checked", this.checked);
+        localStorage.setItem("lista", $(".list").html());
+    });
 
 
 $(".list").html(localStorage.getItem("lista"));
